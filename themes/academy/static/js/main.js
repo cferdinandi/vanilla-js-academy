aafe2da97931cc659f6dd31b18901e9fdd0ff75a
cferdinandi <chris@gomakethings.com>
2018-04-15 15:23:14 -0400

    Removed unused scripts

--- themes/academy/static/js/main.js
+++ themes/academy/static/js/main.js
@@ -6,13 +6,6 @@
  * Open Source Credits: https://github.com/toddmotto/fluidvids, http://prismjs.com, https://github.com/filamentgroup/loadJS/, https://github.com/filamentgroup/loadCSS, https://github.com/bramstein/fontfaceobserver
  */
 
-/**
- * Element.matches() polyfill (simple version)
- * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
- */
-if (!Element.prototype.matches) {
-	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-}
 ;(function (window, document, undefined) {
 
 	'use strict';
@@ -308,82 +301,6 @@ var mailchimp = function (callback) {
 
 	form.setAttribute('novalidate', 'novalidate');
 	form.addEventListener('submit', submitHandler, false);
-
-};
-/**
- * Load pricing parity message
- */
-var pricingParity = function (endpoint, template) {
-
-	'use strict';
-
-	// Make sure endpoint and template exist
-	if (!endpoint) return;
-
-	// Render the pricing parity message
-	var renderPricingParity = function (data) {
-
-		// Make sure we have data to render
-		if (!data || !template) return;
-
-		// Convert data to JSON
-		data = JSON.parse(data);
-
-		// Make sure discount exists
-		if (data.status === 'no_discount') return;
-
-		// Get the nav
-		var nav = document.querySelector('header');
-		if (!nav) return;
-
-		// Create container
-		var pricing = document.createElement('div');
-		pricing.id = 'pricing-parity';
-		pricing.className = 'bg-muted padding-top-small padding-bottom-small';
-		pricing.innerHTML = template.replace('{{iso}}', data.code).replace('{{country}}', data.country).replace('{{code}}', data.discount).replace('{{amount}}', data.amount);
-
-		// Insert into the DOM
-		nav.parentNode.insertBefore(pricing, nav);
-
-	};
-
-	// Get the pricing parity message via Ajax
-	var getPricingParity = function () {
-
-		// Set up our HTTP request
-		var xhr = new XMLHttpRequest();
-		if (!('responseType' in xhr)) return;
-
-		// Setup our listener to process compeleted requests
-		xhr.onreadystatechange = function () {
-			// Only run if the request is complete
-			if (xhr.readyState !== 4) return;
-
-			// Process our return data
-			if (xhr.status === 200) {
-
-				// Save the content to sessionStorage
-				sessionStorage.setItem('gmt-pricing-parity', xhr.response);
-
-				// Render it
-				renderPricingParity(xhr.response);
-
-			}
-		};
-
-		// Create and send a GET request
-		xhr.open('GET', endpoint);
-		xhr.send();
-
-	};
-
-	// Get and render pricing parity info
-	var pricing = sessionStorage.getItem('gmt-pricing-parity');
-	if (typeof pricing === 'string') {
-		renderPricingParity(pricing);
-	} else {
-		getPricingParity();
-	}
 
 };
 /*!
@@ -1036,6 +953,3 @@ if (document.querySelector('#mailchimp-form')) {
 		}
 	}));
 }
-
-// Pricing parity
-pricingParity('https://gomakethings.com/checkout/wp-json/gmt-pricing-parity/v1/discount/', '<div class="container container-large"><img width="100" style="float:left;margin: 0 16px 16px 0;" src="https://flagpedia.net/data/flags/normal/{{iso}}.png"><p class="text-small no-margin-bottom">Hi! Looks like you\'re from <strong>{{country}}</strong>, where my <strong>Vanilla JS Pocket Guides</strong> might be a bit expensive. You can use the code <strong>{{code}}</strong> at checkout to take <strong>{{amount}}</strong> off any guide, course, or bundle. Cheers!</p></div>');
