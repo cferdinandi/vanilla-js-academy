c54ca17e1b0dc747bf6a07a00de90f9be8a3c755
cferdinandi <chris@gomakethings.com>
2018-08-27 09:51:29 -0400

    Basic updates

---
+++ content/signup-success.md
@@ -0,0 +1,13 @@
+---
+title: "You're on the list!"
+url: "/registration-success"
+date: 2018-01-11T16:03:33-05:00
+draft: false
+---
+
+Thanks for signing up to get notifications and exclusive discounts for the Vanilla JS Academy. You'll be the first to hear when the next session opens up, and get special discounts and behind the scenes info.
+
+If you have any questions, comments, or feedback while you wait, send me an email at {{<email>}}.
+
+Cheers,<br>
+Chris
--- themes/academy/src/js/main/zzz_init.js
+++ themes/academy/src/js/main/zzz_init.js
@@ -17,10 +17,13 @@ if (document.querySelector('a[href*="#"]')) {
 if (document.querySelector('#mailchimp-form')) {
 	mailchimp(function (data) {
 		if (data.code === 200) {
-			window.location.href = 'https://gomakethings.com/newsletter-success';
+			window.location.href = '/signup-success';
 		}
 	});
 }
 
 // API for testimonials and CTAs
 api();
+
+// Pricing parity
+// pricingParity('https://gomakethings.com/checkout/wp-json/gmt-pricing-parity/v1/discount/', '<div class="container container-large"><img width="100" style="float:left;margin: 0 16px 16px 0;" src="https://flagpedia.net/data/flags/normal/{{iso}}.png"><p class="text-small no-margin-bottom">Hi! Looks like you\'re from <strong>{{country}}</strong>, where my <strong>Vanilla JS Pocket Guides</strong> might be a bit expensive. You can use the code <strong>{{code}}</strong> at checkout to take <strong>{{amount}}</strong> off any guide, course, or bundle. Cheers!</p></div>');
