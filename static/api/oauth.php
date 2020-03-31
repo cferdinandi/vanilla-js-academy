f7dd5d620ff6f3a22776a38c945018631c6736fa
cferdinandi <chris@gomakethings.com>
2020-03-31 16:43:26 -0400

    Publish new oauth api

---
+++ static/api/oauth.php
@@ -0,0 +1,13 @@
+<?php
+
+	if (empty($_POST['email'])) {
+		http_response_code(500);
+		die('Please provide a valid email address.');
+	}
+
+	http_response_code(200);
+	echo(json_encode(array(
+		'access_token' => md5($_POST['email']),
+		'expires' => 1000 * 60 * 60 * 24,
+		'token_type' => 'Bearer'
+	)));
