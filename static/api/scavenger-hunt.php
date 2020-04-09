5b6559841152bcfc5b78bece8d076633018ddc5f
cferdinandi <chris@gomakethings.com>
2020-04-09 10:00:07 -0400

    Removed auth on get

--- static/api/scavenger-hunt.php
+++ static/api/scavenger-hunt.php
@@ -12,12 +12,6 @@
 	// GET Request
 	if ($method === 'GET') {
 
-		// If there's no matching user, bail
-		if (empty($path) && empty($_GET['public'])) {
-			http_response_code(403);
-			die('You shall not pass!');
-		}
-
 		// Get the file
 		$file = get_file(empty($path) ? $_GET['public'] : $path, 'scavenger-hunt', 'scavenger-hunt-items.json', true);
 
