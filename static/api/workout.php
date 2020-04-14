ef7b7b2c61604adad36e244a9d6f84e71bc595c8
cferdinandi <chris@gomakethings.com>
2020-04-14 17:42:04 -0400

    debugging

--- static/api/workout.php
+++ static/api/workout.php
@@ -36,23 +36,15 @@
 		// Get the file
 		$file = get_file($path, 'workout', array('workouts' => array()));
 
-		http_response_code(200);
-		die(json_encode(array(
-			'file' => $file,
-			'type' => gettype($file),
-			'typeWorkouts' => gettype($file['workouts']),
-			'workouts' => $file['workouts']
-		)));
-
 		// Check if item already exists
 		$existing = find_by_key_value('id', $_POST['id'], $file->{'workouts'});
 
 		// If the item doesn't exist, create it
 		// Otherwise, replace it
 		if ($existing === false) {
-			$file->{'workouts'}[] = $_POST;
+			$file['workouts'][] = $_POST;
 		} else {
-			$file->{'workouts'}[$existing] = $_POST;
+			$file['workouts'][$existing] = $_POST;
 		}
 
 		// Save to database
