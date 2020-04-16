dc901c3374d33e18f7440fd9845edd7dd62f171e
cferdinandi <chris@gomakethings.com>
2020-04-16 12:23:58 -0400

    Updated path

--- static/api/workout.php
+++ static/api/workout.php
@@ -6,11 +6,11 @@
 	// Get the API method & workout ID
 	$method = get_method();
 	$id = $_GET['id'];
-	$advanced = $_GET['weight'];
 
 	// Authenticate user
-	$path = get_path_from_token() . (empty($advanced) ? '' : '_weight');
+	$path = get_path_from_token();
 	authenticate_user($path);
+	$filepath = $path . (empty($_GET['weight']) ? '' : '_weight');
 
 	// GET Request
 	if ($method === 'GET') {
@@ -36,13 +36,13 @@
 		}
 
 		// Get the file
-		$file = get_file($path, 'workout', new stdClass());
+		$file = get_file($filepath, 'workout', new stdClass());
 
 		// Add or update the item
 		$file->{$id} = $_POST;
 
 		// Save to database
-		set_file($path, 'workout', $file);
+		set_file($filepath, 'workout', $file);
 
 		// Return data
 		http_response_code(200);
@@ -55,19 +55,19 @@
 
 		// If no ID was provided, reset entire file
 		if (empty($id)) {
-			set_file($path, 'workout', new stdClass());
+			set_file($filepath, 'workout', new stdClass());
 			http_response_code(200);
 			die(json_encode(new stdClass()));
 		}
 
 		// Get the file
-		$file = get_file($path, 'workout', new stdClass());
+		$file = get_file($filepath, 'workout', new stdClass());
 
 		// Delete the item
 		unset($file->{$id});
 
 		// Save to database
-		set_file($path, 'workout', $file);
+		set_file($filepath, 'workout', $file);
 
 		// Return data
 		http_response_code(200);
